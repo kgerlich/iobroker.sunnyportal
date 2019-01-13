bb58ce732008570144e54abba25bd7364171007c
Klaus Gerlicher <klaus@klausgerlicher.de>
2019-01-13 13:20:46 +0100

    try to fix login; add some more dynamic content to admin page; add error/info/warnings

--- main.js
+++ main.js
@@ -81,7 +81,6 @@ function create_indicator(name, description, value) {
 }
 
 function main() {
-
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
     adapter.log.info('config username: ' + adapter.config.username);
@@ -91,19 +90,99 @@ function main() {
     // in this all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 
-    login();
+    setup();
 }
 
 var jar = request.jar();
 const url = 'https://sunnyportal.com';
+var timer = 0;
 
-function login() {
+function reset() {
+    if (timer != 0) {
+        clearInterval(timer);
+        timer = 0;
+    } 
+    setTimeout(setup, 5*1000);
+}
+
+function homemanager(httpResponse) {
+    var HOMEMANAGER_URL = '/homemanager';
+    var requestOpts = {
+        headers: {
+            'Referer' : 'https://www.sunnyportal.com/FixedPages/HoManLive.aspx',
+            "DNT" : 1,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        // Service does not have a valid cert
+        strictSSL : false,
+        jar : jar
+    };
+
+    var d = new Date();
+    var n = d.getTime();
+
+    request.get(url + HOMEMANAGER_URL + '?t=' + n, requestOpts, function (err, httpResponse, body) {
+        if (err) {
+            adapter.log.debug('request failed:', err);
+            reset();
+            return;
+        }
+            
+        try {
+            var obj = JSON.parse(body);
+        } catch (error) {
+            adapter.log.debug('error in JSON!');
+            reset();
+            return;
+        }
+
+        adapter.log.debug(JSON.stringify(obj));
+        create_indicator('infos', 'info messages', null);
+        obj.InfoMessages.forEach(function(element, index) {
+            adapter.log.info("info[" + index + "] " + element);
+            create_indicator('infos.' + index, 'info message ' + index, element);
+        });
+        create_indicator('warnings', 'warning messages', null);
+        obj.WarningMessages.forEach(function(element, index) {
+            adapter.log.info("warning[" + index + "] " + element);
+            create_indicator('warnings.' + index, 'warning message ' + index, element);
+        });
+        create_indicator('errors', 'error messages', null);
+        obj.ErrorMessages.forEach(function(element, index) {
+            adapter.log.info("errors[" + index + "]" + element);
+            create_indicator('errors.' + index, 'error message ' + index, element);
+        });
+
+
+        create_indicator('total_consumption', 'total consumption', obj.TotalConsumption);
+        create_indicator('grid_consumption', 'grid consumption', obj.GridConsumption);
+        create_indicator('self_consumption', 'self consumption', obj.SelfConsumption);
+        create_indicator('self_consumption_quote', 'self consumption quote', obj.SelfConsumptionQuote);
+        create_indicator('autarky_quote', 'autarky quote', obj.AutarkyQuote);
+        if (!obj.Timestamp) {
+            adapter.log.debug('no data!');
+            reset();
+            return;
+        }
+        create_indicator('last_update', 'time of last update', obj.Timestamp.DateTime);
+
+        if (timer == 0) {
+            timer = setInterval(homemanager, (adapter.config.interval >= 15) ? adapter.config.interval * 1000 : 15*1000);
+        }
+    });
+}
+
+function setup() {
+    login(homemanager);
+}
+
+function login(callback) {
     var LOGIN_URL = '/Templates/Start.aspx';
     
     var username = adapter.config.username;
     var password = adapter.config.password;
     var plantOID = adapter.config.plantoid;
-    
     
     var requestOpts = {
         headers: {
@@ -136,52 +215,10 @@ function login() {
     
     request.post(url + LOGIN_URL, requestOpts, function (err, httpResponse, body) {
         if (err) {
-            console.error('login failed:', err);
+            adapter.log.debug('login failed, retrying in 5 sec: ', err);
+            setTimeout(setup, 5*1000);
             return ;
         }
-        homemanager();
-        setInterval(homemanager, (adapter.config.interval >= 15) ? adapter.config.interval * 1000 : 15*1000);
+        callback();
     });
-}
-
-function homemanager(httpResponse) {
-    var HOMEMANAGER_URL = '/homemanager';
-    var requestOpts = {
-        headers: {
-            'Referer' : 'https://www.sunnyportal.com/FixedPages/HoManLive.aspx',
-            "DNT" : 1,
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-            'X-Requested-With': 'XMLHttpRequest'
-        },
-        // Service does not have a valid cert
-        strictSSL : false,
-        jar : jar
-    };
-
-    var d = new Date();
-    var n = d.getTime();
-
-    request.get(url + HOMEMANAGER_URL + '?t=' + n, requestOpts, function (err, httpResponse, body) {
-        if (err) {
-            console.error('login failed:', err);
-            return ;
-        }
-        var obj = JSON.parse(body);
-
-        if (!obj.TotalConsumption) {
-            adapter.log.debug('no data!');
-            return;
-        }
-
-        adapter.log.debug(JSON.stringify(obj));
-        adapter.log.info('Total consumption: ' + obj.TotalConsumption + ' (' + obj.Timestamp.DateTime + ')' );
-
-        create_indicator('total_consumption', 'total consumption', obj.TotalConsumption);
-        create_indicator('grid_consumption', 'grid consumption', obj.GridConsumption);
-        create_indicator('self_consumption', 'self consumption', obj.SelfConsumption);
-        create_indicator('self_consumption_quote', 'self consumption quote', obj.SelfConsumptionQuote);
-        create_indicator('autarky_quote', 'autarky quote', obj.AutarkyQuote);
-        create_indicator('last_update', 'time of last update', obj.Timestamp.DateTime);
-    });
-       
 }
